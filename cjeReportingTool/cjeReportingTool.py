2c5a37b4133a9fcb04d58b33651cb8a6875a03b6
eggplants <w10776e8w@yahoo.co.jp>
2021-01-27 21:07:12 +0900

    cli

--- cjeReportingTool/cjeReportingTool.py
+++ cjeReportingTool/cjeReportingTool.py
@@ -1,48 +1,49 @@
-#========================================== #
+# ==========================================#
 # Project Name    : cjeReportingTool
 # File Name       : cjeReportingTool.py
 # Encoding        : utf-8
 # Creation Date   : 26-Jun-2021
- 
 # Copyright © 2021 kami; Takahito Murakami. All rights reserved.
 # This source code or any portion licensed by MIT
-#===========================================#
+# ===========================================#
 
 def cjeReportingTool(path, outpath, split_str, prefix):
-  #文字列へ強制変換
-  path = str(path)
-  outpath = str(outpath)
-  split_str = str(split_str)
-  prefix = str(prefix)
 
-  #シークエンスバー
-  seq, sequence_per, sequence_con= 0,0, 0
-  seq = sum(1 for line in open(path))
+    # 文字列へ強制変換
+    path = str(path)
+    outpath = str(outpath)
+    split_str = str(split_str)
+    prefix = str(prefix)
 
-  #指定ファイルを開く
-  f = open(path, 'r')
-  f2 = open(outpath, 'w')
+    # シークエンスバー
+    seq, sequence_per, sequence_con = 0, 0, 0
+    seq = sum(1 for line in open(path))
 
-  line_cout = 1 #lineのカウントの初期化
-  linenum_explanation = {} #行番号:説明文対辞書
+    # 指定ファイルを開く
+    f = open(path, 'r')
+    f2 = open(outpath, 'w')
 
-  #1行づつ処理して指定語があったらsplitしてsplit_line[1]を辞書に登録
-  for line in f:
-    line = line.rstrip() 
-    sequence_con += 1
-    sequence_per = (sequence_con / seq)*100
-    if split_str in line:
-      split_line = line.split(split_str)
-      line_cout_str = str(line_cout)
-      linenum_explanation[line_cout_str] = str(split_line[1])
-      line_cout += 1
-    else:
-      line_cout += 1
+    line_cout = 1  # lineのカウントの初期化
+    linenum_explanation = {}  # 行番号:説明文対辞書
 
-    print('■', str(round(sequence_per,2)),'%',end='')
+    # 1行づつ処理して指定語があったらsplitしてsplit_line[1]を辞書に登録
+    for line in f:
+        line = line.rstrip()
+        sequence_con += 1
+        sequence_per = (sequence_con / seq) * 100
+        if split_str in line:
+            split_line = line.split(split_str)
+            line_cout_str = str(line_cout)
+            linenum_explanation[line_cout_str] = str(split_line[1])
+            line_cout += 1
+        else:
+            line_cout += 1
 
-  for line_num in linenum_explanation:
-    f2 = open(outpath, 'a')
-    f2.write(prefix +'\t'+ line_num + ':' + '\t' + linenum_explanation[line_num]+ '\t'+'\t'+'\n'+'\n')
-  f.close()
-  f2.close()
+    print('■', str(round(sequence_per, 2)), '%', end='')
+
+    for line_num in linenum_explanation:
+        f2 = open(outpath, 'a')
+        f2.write('{}\t{}:\t{}\t\t\n\n'.format(
+            prefix, line_num, linenum_explanation[line_num]))
+    f.close()
+    f2.close()
---
+++ cjeReportingTool/main.py
@@ -0,0 +1,33 @@
+import argparse
+import textwrap
+
+import cjeReportingTool
+
+
+def parseArgs():
+    """Parse arguments."""
+    parser = argparse.ArgumentParser(
+        prog='cjeReportingTool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        This Tool made for CJE(Chishiki Joho Enshu 3) class at Univ. Tsukuba, klis.
+        This package converts source code comment to markdown text using split words.'''))
+    parser.add_argument('path',  metavar='path', nargs=1,
+                        help='Read file path')
+    parser.add_argument('outpath',  metavar='outpath', nargs=1,
+                        help='Export file path')
+    parser.add_argument('split_str',  metavar='split_str', nargs=1, default='##',
+                        help='A symbol or string that separates comments from source code')
+    parser.add_argument('prefix',  metavar='prefix', nargs=1, default='>',
+                        help='A Prefix to be written out')
+    return parser.parse_args()
+
+
+def main():
+    args = parseArgs()
+    cjeReportingTool.cjeReportingTool(
+       args.path, args.outpath, args.split_str, args.prefix)
+
+
+if __name__ == "__main__":
+    main()
