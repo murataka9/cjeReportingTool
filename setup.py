e07f29225bf421dd5769367619295ffe49179a6a
eggplants <w10776e8w@yahoo.co.jp>
2021-09-15 11:51:47 +0900

    fix: restrict python version (fix: #4)

--- setup.py
+++ setup.py
@@ -13,20 +13,14 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='cjeReportingTool',
     packages=['cjeReportingTool'],
-
     version='2.0.1',
-
     license='MIT',
-
-    # install_requires=['janome', 'pykakasi', 'semidbm', 'six'],
-
     author='kami; Takahito Murakami',
     author_email='takahito@klis.tsukuba.ac.jp',
-
     url='https://github.com/murataka9/cjeReportingTool',
-
     description=('This Tool made for CJE(Chishiki Joho Enshu 3) class at Univ. of Tsukuba, KLIS. '
                  'This package converts source code comment to markdown text using split words.'),
+    python_requires='>=3.6',
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords='cjeReportingTool comment converter CJE University of Tsukuba KLIS',
